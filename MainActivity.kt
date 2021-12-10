37871c666679ccb6e7190f5cc3a1a667e9837187
poisonedapple <s18515@salesio-sp.ac.jp>
2021-12-10 23:16:47 +0900

    21/12/10

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.test2
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         var textMessage = findViewById<TextView>(R.id.textMessage)
         var buttonA = findViewById<Button>(R.id.button_A)
         var buttonB = findViewById<Button>(R.id.button_B)
+        var inputText = findViewById<EditText>(R.id.editTextTextPersonName)
 
         buttonA.setOnClickListener {
             textMessage.setText("abc")
